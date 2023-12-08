80ad7e3333b1db404e455233cc53fb4a2d4e741c
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2023-12-08 13:36:59 +0100

    Double escape, angle fix 7

--- indexer.sh
+++ indexer.sh
@@ -21,7 +21,7 @@ generate_rss() {
   <link>https://blog.wester.digital</link>
   <description>Random blogposts delivered in plaintext</description><item><title>Problems with the RSS feed?</title><description><![CDATA[<pre>Send me an email at <a href=\"mailto:rss@wester.digital\">rss@wester.digital</a> or make an issue on <a href=\"https://github.com/user18130814200115-2/plain.wester.digital\">GitHub</a> with the problem you encountered and I will try to resolve it." > feed.xml
 
-    head -n 9999 posts/* | sed -Ee 's/>/\&gt;/g' -e 's/</\&lt;/g' -e 's/^==.gt. (.*) .lt.==$/<\/pre>]]><\/description><\/item><item><link>https:\/\/blog.wester.digital\1<\/link><title>/g' -e 's/([A-z].., [0-9]* [A-z].. [0-9]...)/<\/title><pubDate>\1<\/pubDate><description><![CDATA[<pre>/g' >> feed.xml
+    head -n 9999 posts/* | sed -Ee 's/>/\&amp;gt;/g' -e 's/</\&amp;lt;/g' -e 's/^==.amp.gt. (.*) .amp.lt.==$/<\/pre>]]><\/description><\/item><item><link>https:\/\/blog.wester.digital\1<\/link><title>/g' -e 's/([A-z].., [0-9]* [A-z].. [0-9]...)/<\/title><pubDate>\1<\/pubDate><description><![CDATA[<pre>/g' >> feed.xml
 
     echo "</pre>]]></description></item></channel></rss>" >> feed.xml
 }
