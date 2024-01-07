119ce0a3ce3f76ba07c29dedf0f5ab2293f29ce5
Thijs Iris Wester <wester.t@protonmail.com>
2024-01-07 17:09:05 +0100

    Translate newpost helper script to python

---
+++ newpost.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import time
+
+number = str(int(sorted(os.listdir("posts"))[-1][0:4]) + 1)
+index = (4 - len(number)) * '0' + number
+filename = 'posts/' + index + '-' + '-'.join(sys.argv[1:]).lower() + '.txt'
+
+title = ' '.join(sys.argv[1:])
+date = time.strftime("%a, %d %b %Y")
+tags = ''
+rule = len(title) * '='
+
+#file = open(filename, "w")
+#file.write(title + '\n' + date + '\n' + tags + '\n' + rule)
+#file.close()
+
+with open(filename, 'w') as file:
+    file.write(title + '\n' + date + '\n' + tags + '\n' + rule)
+
+os.system("vim " + filename)
