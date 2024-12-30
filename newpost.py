3690c810ed3df1d4000e8a13bbd89184012caaa0
Thijs Wester <wester.t@protonmail.com>
2024-12-30 21:50:42 +0100

    Automatically add footer to new posts

--- newpost.py
+++ newpost.py
@@ -13,11 +13,20 @@ date = time.strftime("%a, %d %b %Y")
 tags = ''
 rule = max(len(title), len(date)) * '='
 
+footer = '''\
+All of my writing and software projects are available free of
+charge under CC-BY unless stated otherwise. I do not accept
+monetary donations, but if my work has brought you value I ask
+you to donate to a charitable cause or high-impact fund,
+organisation, business, institute, or individual driving moral
+progress.
+'''
+
 #file = open(filename, "w")
 #file.write(title + '\n' + date + '\n' + tags + '\n' + rule)
 #file.close()
 
 with open(filename, 'w') as file:
-    file.write(title + '\n' + date + '\n' + tags + '\n' + rule)
+    file.write(title + '\n' + date + '\n' + tags + '\n' + rule + '\n' * 3 + footer)
 
 os.system("vim " + filename)
