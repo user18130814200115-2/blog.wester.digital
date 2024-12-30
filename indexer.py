1bdc447f3cc346b94df4a22e8d2b6492c48867eb
Thijs Wester <wester.t@protonmail.com>
2024-12-30 21:48:46 +0100

    Update footer to match post footer

--- indexer.py
+++ indexer.py
@@ -23,12 +23,12 @@ All of my writings are available through the web, gopher and rss:
 
 # To this the email address gets appended formatted in the manner required for the format
 footer = '''\
-All of my writings are available free of charge and I do not
-accept monetary donations. If you are feeling charitable, I
-encourage you to donate to a charitable organisation or
-non-profit either with your money, time, voice, or other form of
-support or to simply help out a someone in need in whatever form
-that may take.
+All of my writing and software projects are available free of
+charge under CC-BY unless stated otherwise. I do not accept
+monetary donations, but if my work has brought you value I ask
+you to donate to a charitable cause or high-impact fund,
+organisation, business, institute, or individual driving moral
+progress.
 
 Email your questions, comments, or recommendations to
 '''
